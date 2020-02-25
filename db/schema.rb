e0ba891b4a6f1fe93dcc7cb960c4b9a1d9f1546b
duy nguyen <duy.nguyen@impl.vn>
2020-02-25 09:10:05 +0700

    fix migration references model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_130505) do
+ActiveRecord::Schema.define(version: 2020_02_25_020544) do
+
+  create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "user_code"
+    t.string "password"
+    t.string "avatar"
+    t.bigint "role_id"
+    t.bigint "details_info_id"
+    t.index ["details_info_id"], name: "index_accounts_on_details_info_id"
+    t.index ["role_id"], name: "index_accounts_on_role_id"
+  end
+
+  create_table "bills", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "creation_time"
+    t.datetime "payment_time"
+    t.bigint "account_id"
+    t.bigint "injection_book_id"
+    t.string "total_money"
+    t.string "code"
+    t.bigint "detail_bill_id"
+    t.index ["account_id"], name: "index_bills_on_account_id"
+    t.index ["detail_bill_id"], name: "index_bills_on_detail_bill_id"
+    t.index ["injection_book_id"], name: "index_bills_on_injection_book_id"
+  end
+
+  create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "company_code"
+    t.string "name"
+    t.string "address"
+  end
+
+  create_table "contract_distributions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "contract_id"
+    t.bigint "details_info_id"
+    t.index ["contract_id"], name: "index_contract_distributions_on_contract_id"
+    t.index ["details_info_id"], name: "index_contract_distributions_on_details_info_id"
+  end
 
   create_table "contract_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "contracts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "start_at"
+    t.datetime "finish_at"
+    t.integer "expire"
+    t.bigint "contract_type_id"
+    t.index ["contract_type_id"], name: "index_contracts_on_contract_type_id"
   end
 
   create_table "departments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -20,8 +65,174 @@ ActiveRecord::Schema.define(version: 2020_02_23_130505) do
     t.string "detail"
   end
 
+  create_table "detail_bills", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "vaccine_id"
+    t.bigint "vaccine_package_id"
+    t.string "unit_price"
+    t.string "amount"
+    t.index ["vaccine_id"], name: "index_detail_bills_on_vaccine_id"
+    t.index ["vaccine_package_id"], name: "index_detail_bills_on_vaccine_package_id"
+  end
+
+  create_table "detail_injection_books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "injection_date"
+    t.string "react_after_injection"
+    t.bigint "vaccination_center_id"
+    t.bigint "account_id"
+    t.bigint "vaccine_id"
+    t.bigint "vaccine_package_id"
+    t.integer "number_injection"
+    t.bigint "injection_book_id"
+    t.index ["account_id"], name: "index_detail_injection_books_on_account_id"
+    t.index ["injection_book_id"], name: "index_detail_injection_books_on_injection_book_id"
+    t.index ["vaccination_center_id"], name: "index_detail_injection_books_on_vaccination_center_id"
+    t.index ["vaccine_id"], name: "index_detail_injection_books_on_vaccine_id"
+    t.index ["vaccine_package_id"], name: "index_detail_injection_books_on_vaccine_package_id"
+  end
+
+  create_table "detail_vaccine_packages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "vaccine_id"
+    t.integer "total_injections"
+    t.index ["vaccine_id"], name: "index_detail_vaccine_packages_on_vaccine_id"
+  end
+
+  create_table "details_infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "identify"
+    t.string "number_phone"
+    t.string "permanent_address"
+    t.string "current_address"
+    t.string "bank"
+    t.string "bank_account_number"
+    t.string "language"
+    t.string "certificate"
+    t.string "experience"
+    t.string "nationality"
+    t.string "gender"
+    t.string "tax_code"
+    t.bigint "vaccination_center_id"
+    t.bigint "department_id"
+    t.index ["department_id"], name: "index_details_infos_on_department_id"
+    t.index ["vaccination_center_id"], name: "index_details_infos_on_vaccination_center_id"
+  end
+
+  create_table "import_vaccines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "company_code"
+    t.integer "quantity"
+    t.bigint "vaccine_id"
+    t.bigint "account_id"
+    t.datetime "created_at"
+    t.index ["account_id"], name: "index_import_vaccines_on_account_id"
+    t.index ["vaccine_id"], name: "index_import_vaccines_on_vaccine_id"
+  end
+
+  create_table "info_injection_books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "father_name"
+    t.string "identify_father"
+    t.string "mother_name"
+    t.string "identify_mother"
+    t.string "number_phone"
+    t.string "current_address"
+    t.string "permanent_address"
+  end
+
+  create_table "injection_books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "book_code"
+    t.string "name_person_injected"
+    t.datetime "date_of_birth"
+    t.string "place_of_birth"
+    t.bigint "info_injection_book_id"
+    t.index ["info_injection_book_id"], name: "index_injection_books_on_info_injection_book_id"
+  end
+
+  create_table "injection_schedules", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "vaccination_center_id"
+    t.datetime "injection_date"
+    t.datetime "registration_date"
+    t.bigint "vaccine_package_id"
+    t.string "book_code"
+    t.index ["vaccination_center_id"], name: "index_injection_schedules_on_vaccination_center_id"
+    t.index ["vaccine_package_id"], name: "index_injection_schedules_on_vaccine_package_id"
+  end
+
   create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "role"
   end
 
+  create_table "vaccination_centers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "code"
+    t.string "address"
+    t.string "number_phone"
+  end
+
+  create_table "vaccine_distributions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "vaccine_id"
+    t.bigint "vaccination_center_id"
+    t.index ["vaccination_center_id"], name: "index_vaccine_distributions_on_vaccination_center_id"
+    t.index ["vaccine_id"], name: "index_vaccine_distributions_on_vaccine_id"
+  end
+
+  create_table "vaccine_package_types", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "vaccine_packages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "content"
+    t.string "code"
+    t.string "total_injections"
+    t.string "price"
+    t.integer "vaccine_package_type_id"
+    t.bigint "detail_vaccine_package_id"
+    t.index ["detail_vaccine_package_id"], name: "index_vaccine_packages_on_detail_vaccine_package_id"
+    t.index ["vaccine_package_type_id"], name: "index_vaccine_packages_on_vaccine_package_type_id"
+  end
+
+  create_table "vaccine_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "vaccines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.string "manufacture"
+    t.datetime "date_of_application"
+    t.string "expiry_date"
+    t.string "content"
+    t.string "date_added"
+    t.string "inventory"
+    t.bigint "vaccine_type_id"
+    t.index ["vaccine_type_id"], name: "index_vaccines_on_vaccine_type_id"
+  end
+
+  add_foreign_key "accounts", "details_infos"
+  add_foreign_key "accounts", "roles"
+  add_foreign_key "bills", "accounts"
+  add_foreign_key "bills", "detail_bills"
+  add_foreign_key "bills", "injection_books"
+  add_foreign_key "contract_distributions", "contracts"
+  add_foreign_key "contract_distributions", "details_infos"
+  add_foreign_key "contracts", "contract_types"
+  add_foreign_key "detail_bills", "vaccine_packages"
+  add_foreign_key "detail_bills", "vaccines"
+  add_foreign_key "detail_injection_books", "accounts"
+  add_foreign_key "detail_injection_books", "injection_books"
+  add_foreign_key "detail_injection_books", "vaccination_centers"
+  add_foreign_key "detail_injection_books", "vaccine_packages"
+  add_foreign_key "detail_injection_books", "vaccines"
+  add_foreign_key "detail_vaccine_packages", "vaccines"
+  add_foreign_key "details_infos", "departments"
+  add_foreign_key "details_infos", "vaccination_centers"
+  add_foreign_key "import_vaccines", "accounts"
+  add_foreign_key "import_vaccines", "vaccines"
+  add_foreign_key "injection_books", "info_injection_books"
+  add_foreign_key "injection_schedules", "vaccination_centers"
+  add_foreign_key "injection_schedules", "vaccine_packages"
+  add_foreign_key "vaccine_distributions", "vaccination_centers"
+  add_foreign_key "vaccine_distributions", "vaccines"
+  add_foreign_key "vaccine_packages", "detail_vaccine_packages"
+  add_foreign_key "vaccine_packages", "vaccine_package_types"
+  add_foreign_key "vaccines", "vaccine_types"
 end
