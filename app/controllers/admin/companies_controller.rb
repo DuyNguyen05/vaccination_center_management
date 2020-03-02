2d7cbb7a1dfe4f19bf9022da0189d341c0a50a6e
duy nguyen <duy.nguyen@impl.vn>
2020-03-02 08:46:27 +0700

    CRUD admin company

---
+++ app/controllers/admin/companies_controller.rb
@@ -0,0 +1,26 @@
+class Admin::CompaniesController < Admin:AdminController
+
+  def index
+    @companies = Company.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     namespace :admin do
       root "home#index"
+      resources :companies
     end
   end
 end
