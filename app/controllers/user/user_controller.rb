251611e0bd031e9d1729eb575795ec19126331b1
PhuongThao <phuongthao.hvktqs14@gmail.com>
2020-03-03 18:43:49 +0700

    fix comment

--- app/controllers/user/user_controller.rb
+++ app/controllers/user/user_controller.rb
@@ -1,15 +1,3 @@
-class User::UserController < ActionController::Base
+class User::UserController < ApplicationController
   layout "user/application"
-  before_action :set_locale
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-
-  private
-
-  def default_url_options
-    {locale: I18n.locale}
-  end
-
 end
