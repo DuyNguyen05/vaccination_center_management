759d5a73a5d5c1e109a8787463e15a52929de853
duy nguyen <duy.nguyen@impl.vn>
2020-05-11 11:37:50 +0700

    admin department

---
+++ app/controllers/admin/departments_controller.rb
@@ -0,0 +1,60 @@
+class Admin::DepartmentsController < Admin::AdminController
+  before_action :set_department, except: [:index, :new, :create]
+
+  def index
+    @departments = Department.page(params[:page])
+  end
+
+  def new
+    @department = Department.new
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def create
+    @department = Department.new department_params
+    respond_to do |format|
+      if @department.save
+        @departments = Department.page(params[:page])
+        format.js
+      else
+        format.js
+      end
+    end
+  end
+
+  def edit
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @department.update department_params
+        @departments = Department.page(params[:page])
+        format.js
+      else
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    @department.destroy
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def department_params
+    params.require(:department).permit(:name, :detail)
+  end
+
+  def set_department
+    @department = Department.find params[:id]
+  end
+end
--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,7 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable
 
   belongs_to :role, optional: true
-  belongs_to :details_info, optional: true
+  belongs_to :details_info
   belongs_to :info_injection_book, optional: true
 
   has_many :bills, dependent: :destroy
