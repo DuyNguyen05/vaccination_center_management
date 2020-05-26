dae4cac794b79d6e9d1818cbec38f8cacd0724e5
Duy Nguyen <43009261+DuyNguyen05@users.noreply.github.com>
2020-05-26 17:11:34 +0700

    hotfix (#30)
    Co-authored-by: duy nguyen <duy.nguyen@impl.vn>

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,6 @@
 class Department < ApplicationRecord
   has_many :details_infos, dependent: :nullify
+  has_many :accounts, through: :details_infos
 
   validates :name, presence: true, uniqueness: true
 end
