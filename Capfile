c6430d98222abbec87bc0e917f20d085af1f6c0d
duy nguyen <duy.nguyen@impl.vn>
2020-08-17 14:02:27 +0700

    test

--- Capfile
+++ Capfile
@@ -8,6 +8,7 @@ require "capistrano/bundler"
 require "capistrano/rails/assets"
 require "capistrano/rails/migrations"
 
+
 install_plugin Capistrano::SCM::Git
 
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :deploy_user, "duy_nguyen"
-server "54.255.211.110", user: "duy_nguyen", roles: %w(app db web)
+server "13.212.59.64", user: "duy_nguyen", roles: %w(app db web)
 set :ssh_options, { forward_agent: false, auth_methods: %w(publickey) }
 
 set :repo_url, "git@github.com:DuyNguyen05/vaccination_center_management.git"
@@ -13,18 +13,32 @@ set :deploy_to, "/home/duy_nguyen/cap/vaccine-center-management"
 append :linked_files, ".env", "config/database.yml", "config/credentials.yml.enc", "config/master.key"
 
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", "vendor/bundle"
-
+set :rbenv_ruby, '2.6.1'
 set :rbenv_type, :user
 
 set :bundle_path, -> { shared_path.join("vendor", "bundle") }
 
-after "deploy:publishing", "deploy:restart"
+# after "deploy:publishing", "deploy:restart"
 
-namespace :deploy do
-  task :restart do
-    on roles(:app) do
-      execute "sudo service puma restart"
-    end
-  end
-end
+# namespace :deploy do
+#   task :restart do
+#     on roles(:app) do
+#       execute "sudo service puma restart"
+#     end
+#   end
+# end
 
+set :puma_rackup, -> {File.join(current_path, "config.ru")}
+set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
+set :puma_pid, -> {"#{shared_path}/tmp/pids/puma.pid"}
+set :puma_bind, -> {"unix://#{shared_path}/tmp/sockets/puma.sock"}
+set :puma_conf, -> {"#{shared_path}/puma.rb"}
+set :puma_access_log, -> {"#{shared_path}/log/puma_access.log"}
+set :puma_error_log, -> {"#{shared_path}/log/puma_error.log"}
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, "staging"))
+set :puma_threads, [0, 8]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, false
